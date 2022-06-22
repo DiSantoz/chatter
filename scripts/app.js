2167c87e40c49f8bfe90056401e21b62f2a11d95
David Ferreira <dsferreira06@gmail.com>
2022-06-22 19:46:25 -0400

    allow user to change username

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 // dom queries
 const chatList = document.querySelector(".chat-list");
 const newChatForm = document.querySelector(".new-chat");
+const newNameForm = document.querySelector(".new-name");
+const updateMessage = document.querySelector(".update-mssg");
 
 // add new chat
 newChatForm.addEventListener("submit", (e) => {
@@ -12,9 +14,23 @@ newChatForm.addEventListener("submit", (e) => {
     .catch((err) => console.log(err));
 });
 
+// update username
+newNameForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const newName = newNameForm.name.value.trim();
+  chatroom.updateName(newName);
+
+  newNameForm.reset();
+  //   show then hide update message
+  updateMessage.innerText = `Your name was updated to ${newName}`;
+  setTimeout(() => {
+    updateMessage.innerText = ``;
+  }, 2000);
+});
+
 // class instances
 const chatUI = new ChatUI(chatList);
-const chatroom = new Chatroom("general", "shaun");
+const chatroom = new Chatroom("general", "anon");
 
 // get chats & render
 chatroom.getChats((data) => chatUI.render(data));
